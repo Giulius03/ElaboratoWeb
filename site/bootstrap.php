e5cc137b4b87f8487d0c0810a114aa9d89df022b
Simone Cristarelli <s.cristarelli@gmail.com>
2024-12-30 19:51:06 +0100

    Modificato file bootstrap con nome db corretto

--- site/bootstrap.php
+++ site/bootstrap.php
@@ -3,6 +3,6 @@ session_start();
 define("UPLOAD_DIR", "./upload/");
 require_once("utils/functions.php");
 require_once("db/database.php");
-$dbh = new DatabaseHelper("localhost", "root", "", "blogtw", 3306);
+$dbh = new DatabaseHelper("localhost", "root", "", "bugsburnleyshop", 3306);
 $currentLanguage = isset($_COOKIE["Lang"]) ? $_COOKIE["Lang"] : "en";
 ?>
